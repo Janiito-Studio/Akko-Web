a218892033d880c6d3d3a8544b1f656728f292a0
Janiito Studio <janlamarcads@gmail.com>
2026-07-03 15:54:06 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -186,11 +186,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Redirigir URLs tipo /discord, /youtube, etc. al link real de cada red social
     const redirectMap = {
-        instagram: 'https://www.instagram.com/aakkooo_official',
-        twitch:    'https://www.twitch.tv/aakkooo_',
-        tiktok:    'https://www.tiktok.com/@aakkoooo_',
-        youtube:   'https://www.youtube.com/@aakkoou?sub_confirmation=1',
-        discord:   'https://discord.gg/MP5Qrawe75'
+        'instagram': 'https://www.instagram.com/aakkooo_official',
+        'twitch':    'https://www.twitch.tv/aakkooo_',
+        'tiktok':    'https://www.tiktok.com/@aakkoooo_',
+        'youtube':   'https://www.youtube.com/@aakkoou?sub_confirmation=1',
+        'discord':   'https://discord.gg/MP5Qrawe75'
     };
 
     const slug = window.location.pathname.split('/').filter(Boolean).pop().toLowerCase();
